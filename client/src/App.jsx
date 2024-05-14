1cd0337bbef64f1d47b6d6b9818dc52cda70d514
runhab <habfenethio@gmail.com>
2024-05-14 22:49:11 +0300

    Header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,12 @@ import SignIn from './pages/Signin'
 // import SignUp from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
-
+import Header from './components/Header'
 export default function App() {
   return (
 
     <BrowserRouter>
-      
+      <Header/>
       <Routes>
 
         <Route path="/" element={<Home />} />
